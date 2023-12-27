8bb5a7eece54fd231202440595527ef266ed4a3a
Adil Amankulov <adiladil9899@gmail.com>
2023-12-28 00:36:40 +0600

    s

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -67,7 +67,7 @@ class MyApp extends App<any> {
       <>
         <Head>
           <title>
-            {publicRuntimeConfig.SITE_NAME} | Modern Open Source URL shortener.
+            {publicRuntimeConfig.SITE_NAME} | Moderns Open Source URL shortener.
           </title>
         </Head>
         <StoreProvider store={this.store}>
